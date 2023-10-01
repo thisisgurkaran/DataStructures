303adc6317aebc0e447aba0db75bb5b810181408
thisisgurkaran <gurkaran.12@gmail.com>
2023-10-01 00:57:51 -0400

    Update

---
+++ Heaps.ts
@@ -0,0 +1,32 @@
+class MinHeap {
+  private heap: number[] = [];
+
+  // helper function to ge the parent of each Node
+  private parentIndex(i: number): number {
+    return Math.floor((i - 1) / 2);
+  }
+  insert(val: number): void {
+    this.heap.push(val);
+    let i = this.heap.length - 1;
+    while (i > 0 && this.heap[this.parentIndex(i)] > this.heap[i]) {
+      [this.heap[i], this.heap[this.parentIndex(i)]] = [
+        this.heap[this.parentIndex(i)],
+        this.heap[i],
+      ];
+      i = this.parentIndex(i);
+    }
+  }
+}
+
+const myHeap = new MinHeap();
+console.log(myHeap);
+myHeap.insert(50);
+
+myHeap.insert(100);
+
+myHeap.insert(45);
+
+console.log(myHeap);
+
+//    45
+// 100   50
--- ReadMe.md
+++ ReadMe.md
@@ -4,3 +4,8 @@ steps to run project
 
 1. run tsc to compile the project and create source file
 2. go to /lib and do node fileName to run the file
+
+ToPics to study
+classes and access modifiers in ts and java
+OOPS in ts and java
+DS and algos and find
